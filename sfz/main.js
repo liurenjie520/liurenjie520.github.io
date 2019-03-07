a896a0e3470ffdde96fd22dd0f75eb1374e31324
Feller <iamliurenjie@gmail.com>
2019-03-07 16:05:26 +0800

    Update main.js

--- sfz/main.js
+++ sfz/main.js
@@ -1,7 +1,7 @@
 function getIdNo() {
     var coefficientArray = ["7", "9", "10", "5", "8", "4", "2", "1", "6", "3", "7", "9", "10", "5", "8", "4", "2"]; // 加权因子
     var lastNumberArray = ["1", "0", "X", "9", "8", "7", "6", "5", "4", "3", "2"]; // 校验码
-    var address = "420101"; // 住址
+    var address = "310101"; // 住址
     var birthday = "19901201"; // 生日
     var s = Math.floor(Math.random() * 10).toString() + Math.floor(Math.random() * 10).toString() + Math.floor(Math.random() * 10).toString();
     var array = (address + birthday + s).split("");
